50f4783ec89b2cb27e5b519a9fe9a69a52f484ed
ACHBADFATIMA <fatiimaachbad@gmail.com>
2023-11-07 23:23:26 +0100

    Adding new entity :Customer and working with relation

--- bank-account-service/bank-account-service/src/main/java/com/example/bankaccountservice/entities/Customer.java
+++ bank-account-service/bank-account-service/src/main/java/com/example/bankaccountservice/entities/Customer.java
@@ -1,5 +1,6 @@
 package com.example.bankaccountservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,5 +16,6 @@ public class Customer {
     private Long id;
     private String name;
     @OneToMany(mappedBy = "customer")
+    @JsonProperty(access  = JsonProperty.Access.WRITE_ONLY)
     private List<BankAccount> bankAccounts;
 }
